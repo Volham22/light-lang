f6574c657c2fc6bea14996ad28cf8113de6541ae
Corentin <coco41.cm@gmail.com>
2022-04-22 16:23:57 +0200

    desugar: Added more for loop tests

--- compiler/tests/loop_tests.rs
+++ compiler/tests/loop_tests.rs
@@ -1,4 +1,5 @@
 use compiler::{
+    desugar::desugar_ast,
     generation::ir_generator::{create_generator, generate_ir_code_jit},
     lexer::Token,
     parser::{parser::Parser, visitors::Statement},
@@ -145,4 +146,79 @@ fn simple_for_loop_with_body() {
     } else {
         assert!(false, "Parser failed!");
     }
+}
+
+#[test]
+fn for_desugar_with_body() {
+    let source = "for let i: number = 0; i < 10; i = i + 1; { i == 1; }";
+
+    let lexer = Token::lexer(source);
+    let tokens = lexer.collect();
+    let mut parser = Parser::new(tokens);
+
+    if let Some(mut ast) = parser.parse() {
+        let mut type_check = TypeChecker::new();
+        assert!(type_check.check_ast_type(&ast).is_ok());
+        desugar_ast(&mut ast);
+
+        if let Statement::ForStatement(_) = &ast[0] {
+            assert!(false);
+        }
+    } else {
+        assert!(false, "Parser failed!");
+    }
+}
+
+#[test]
+fn for_desugar_no_body() {
+    let source = "for let i: number = 0; i < 10; i = i + 1; {}";
+
+    let lexer = Token::lexer(source);
+    let tokens = lexer.collect();
+    let mut parser = Parser::new(tokens);
+
+    if let Some(mut ast) = parser.parse() {
+        let mut type_check = TypeChecker::new();
+        assert!(type_check.check_ast_type(&ast).is_ok());
+        desugar_ast(&mut ast);
+
+        if let Statement::ForStatement(_) = &ast[0] {
+            assert!(false);
+        }
+    } else {
+        assert!(false, "Parser failed!");
+    }
+}
+
+#[test]
+fn for_missing_semicolon() {
+    let source = "for let i: number = 0 i < 10; i = i + 1; {}";
+
+    let lexer = Token::lexer(source);
+    let tokens = lexer.collect();
+    let mut parser = Parser::new(tokens);
+
+    assert!(parser.parse().is_none());
+}
+
+#[test]
+fn for_no_loop_condition() {
+    let source = "for let i: number = 0; i = i + 1; {}";
+
+    let lexer = Token::lexer(source);
+    let tokens = lexer.collect();
+    let mut parser = Parser::new(tokens);
+
+    assert!(parser.parse().is_none());
+}
+
+#[test]
+fn for_no_next_statement() {
+    let source = "for let i: number = 0; i < 10; {}";
+
+    let lexer = Token::lexer(source);
+    let tokens = lexer.collect();
+    let mut parser = Parser::new(tokens);
+
+    assert!(parser.parse().is_none());
 }
